98fd9bbbdad8f73c8d3d86c0d7720f1ec2907619
Ruoying Hao <ruoyinghao@outlook.com>
2016-06-23 10:38:16 +0800

    Create init.sql

---
+++ SQL-PY-Practice/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE A(
+ID varchar(10) PRIMARY KEY,
+ANAME varchar(8)  NOT NULL, 
+QTY varchar(14), 
+CITY varchar(20)
+);
+INSERT INTO A VALUES('A2','Anne','100','Chicago'),
+('A3','Tom','300','Austin'),
+('A1','Jack','200','New york'),
+('A4','Katie','400','Houston'),
+('A6','Eric','300','Richmond');
